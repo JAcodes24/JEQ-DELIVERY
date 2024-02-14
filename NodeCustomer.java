1b167a942f13c3123aeb1b91cd54b5065914c89f
JAcodes24 <160045434+JAcodes24@users.noreply.github.com>
2024-02-15 00:17:55 +0800

    Add files via upload

---
+++ NodeCustomer.java
@@ -0,0 +1,11 @@
+  
+public class NodeCustomer
+{
+    public Customer data;
+    public NodeCustomer next;
+    
+    public NodeCustomer (Customer element)
+    {
+        data = element;
+    }
+}
